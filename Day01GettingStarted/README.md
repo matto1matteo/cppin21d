b925ff5e44a132294be250c680668028d458080e
Matteo Maria Emanuele Cialei <matteome.cialei@gmail.com>
2023-03-12 23:33:21 +0100

    day 1 readme and "full" example

---
+++ Day01GettingStarted/README.md
@@ -0,0 +1,5 @@
+# Day 1
+
+First day of the first week shows and tells the very basic concept of
+programming, such as what a compiler is and how to write some sample code in
+c++.
--- Day01GettingStarted/main.cpp
+++ Day01GettingStarted/main.cpp
@@ -1,7 +1,10 @@
+//include <iostream> // directives needs `#`
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
+    // std::cout << endl; // Use of undeclared identifier
     std::cout << "Hello world!\n";
+    // std::cout << end; // Some function redefinition error
     return 0;
 }
